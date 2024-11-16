ec9639a91a6531f4f9fde05b05914f8b7c3beb04
Shlok Kohli <shlokkohli11@gmail.com>
2024-11-16 23:34:20 +0530

    50% SignUp form complete

---
+++ frontend/src/components/Input.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Input({icon: Icon, type, placeholder, value, onChange}) {
+  return (
+    <div className='relative mb-6 mx-auto'>
+
+        <div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
+            <Icon className='size-5 text-green-500' />
+        </div>
+
+        <input
+        className='w-full pl-10 pr-3 py-2 bg-gray-800 bg-opacity-50 rounded-lg border border-gray-700 focus:border-green-500 focus:ring-2 focus:ring-green-500 text-white placeholder-gray-400 transition duration-200'
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        />
+
+        {/* Password strengeth meter */}
+
+    </div>
+  )
+}
+
+export default Input
